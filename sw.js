19cf839ae044bbb8df3ce2710648f5e8b9774c5d
MATRIXOF <danrleyfelipe58@gmail.com>
2026-03-26 22:35:13 -0300

    Update service worker to version 2 and improve formatting

--- sw.js
+++ sw.js
@@ -1,5 +1,11 @@
-const CACHE='slotpulse-v1';
-const ASSETS=['./','/index.html','/manifest.json','/icon-192x192.png','/icon-512x512.png'];
+const CACHE='slotpulse-v2';
+const ASSETS=[
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon-192x192.png',
+  '/icon-512x512.png'
+];
 
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
@@ -7,12 +13,18 @@ self.addEventListener('install',e=>{
 });
 
 self.addEventListener('activate',e=>{
-  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
+  e.waitUntil(
+    caches.keys().then(keys=>
+      Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))
+    )
+  );
   self.clients.claim();
 });
 
 self.addEventListener('fetch',e=>{
   e.respondWith(
-    caches.match(e.request).then(cached=>cached||fetch(e.request).catch(()=>caches.match('/index.html')))
+    caches.match(e.request).then(cached=>
+      cached||fetch(e.request).catch(()=>caches.match('/index.html'))
+    )
   );
 });
