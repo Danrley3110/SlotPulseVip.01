8150138fac4d25d97f5ae116851fadfe45bdc960
MATRIXOF <danrleyfelipe58@gmail.com>
2026-03-29 15:17:25 -0300

    Update service worker cache version and fetch handling

--- sw.js
+++ sw.js
@@ -1,18 +1,24 @@
-const CACHE='slotpulse-v3';
+const CACHE='slotpulse-v4';
+const ASSETS=['./','./index.html','./manifest.json','./icon-192x192.png','./icon-512x512.png'];
 
 self.addEventListener('install',e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS).catch(()=>{})));
   self.skipWaiting();
 });
 
 self.addEventListener('activate',e=>{
   e.waitUntil(
-    caches.keys().then(keys=>
-      Promise.all(keys.map(k=>caches.delete(k)))
-    )
+    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
   );
   self.clients.claim();
 });
 
 self.addEventListener('fetch',e=>{
-  e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));
+  e.respondWith(
+    fetch(e.request).then(r=>{
+      const rc=r.clone();
+      caches.open(CACHE).then(c=>c.put(e.request,rc));
+      return r;
+    }).catch(()=>caches.match(e.request))
+  );
 });
